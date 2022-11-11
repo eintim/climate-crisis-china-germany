06ac1c63b1b73e2579a526762604347efac9077b
eintim <tim.horlacher@protonmail.com>
2022-11-11 21:00:39 +0100

    Mapped a few countries

--- map.r
+++ map.r
@@ -7,12 +7,25 @@ energy <- readr::read_csv('https://nyc3.digitaloceanspaces.com/owid-public/data/
 # Load map data
 world_map <- map_data("world")
 
-# Filter data 2021
-energy.2021 <- filter(energy, year == 2021)
+# Filter data
+energy.2021 <- energy %>%
+  filter(year == 2015) %>%  # Keep data for 2021
+  select(country, energy_per_capita) %>%  # Select the two columns of interest
+  rename(region = country) %>%  # Rename column
+  # Replace "United States of America" by USA in the region column
+  mutate(
+    region = ifelse(region == "United States", "USA", region),
+    region = ifelse(region == "United Kingdom", "UK", region),
+    region = ifelse(region == "Congo", "Republic of Congo", region),
+    region = ifelse(region == "Czechia", "Czech Republic", region)
+    #region = ifelse(region == "????", "Democratic Republic of the Congo", region),
+    #region = ifelse(region == "?????", "?????", region) #Elfenbeinküste
+  )  
 
 # Merge data
-energy_map.2021 <- left_join(world_map, energy.2021, by= c("region" = "country"))
+energy_map.2021 <- left_join(world_map, energy.2021, by= c("region"))
 
 # Draw Map
 ggplot(energy_map.2021, aes(long, lat, group = group))+
   geom_polygon(aes(fill = energy_per_capita ), color = "white")
+
