213ab95e01dd28d8abc333b5147c033f4ae14037
eintim <tim.horlacher@protonmail.com>
2023-01-04 13:31:35 +0100

    Added map plot

--- map.r
+++ map.r
@@ -2,11 +2,31 @@ library(ggplot2)
 library(dplyr)
 library(gganimate)
 library(gifski)
+library(tidyverse)
 library(transformr)
 
 # Load data
 energy <- readr::read_csv('https://nyc3.digitaloceanspaces.com/owid-public/data/energy/owid-energy-data.csv')
 co2 <- readr::read_csv('https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv')
+
+
+library(sf)
+library(giscoR)
+
+EU_countries <- gisco_get_countries(region = "Europe") %>%
+  st_transform(3035)
+
+test <-co2 %>%
+  filter(year == 2019) %>%
+  select(iso_code, co2) %>%
+  right_join(EU_countries, by = c("iso_code" = "ISO3_CODE"))
+
+test %>% ggplot() +
+  geom_sf(aes(geometry = geometry, fill = co2)) +
+  xlim(c(2200000, 7150000)) +
+  ylim(c(1380000, 5500000)) +
+  labs(title = "CO₂ Emissionen in 2019 in Europa")
+
 
 # Filter data
 energy.2019 <- energy %>%
@@ -36,18 +56,15 @@ co2.china %>%
 epsg_code <- 3035
 EU_countries <- gisco_get_countries(region = "Europe") %>%
   st_transform(epsg_code)
-EU_countries.energy <- left_join(EU_countries, energy.2019, by= c("ISO3_CODE" = "iso_code"))
-ggplot(EU_countries.energy) +
+
+energy %>%
+  filter(year == 2019) %>%  # Keep data for 2019
+  select(country, iso_code, energy_per_capita) %>%
+left_join(EU_countries, ., by= c("ISO3_CODE" = "iso_code")) %>%
+ggplot() +
   geom_sf(aes(fill = energy_per_capita)) +
   xlim(c(2200000, 7150000)) +
-  ylim(c(1380000, 5500000)) +
-  labs(
-    title = "Energy per Capita in 2019",
-    subtitle = "Europe",
-    caption = paste0(
-      "Source: OurWorldInData.org/energy/, ", gisco_attributions()
-    )
-  )
+  ylim(c(1380000, 5500000))
 
 # Whole world
 All_countries <- gisco_get_countries() 
@@ -65,12 +82,28 @@ ggplot(All_countries.energy) +
 
 
 #Gif Stuff
+EU_countries <- gisco_get_countries(region = "Europe") %>%
+  st_transform(3035)
+
+co2 %>%
+  filter(year > 1999) %>%
+  select(iso_code, year, co2_per_capita) %>%
+  left_join(EU_countries, ., by = c("ISO3_CODE" = "iso_code")) %>%
+  ggplot() +
+  geom_sf(aes(fill = co2_per_capita)) +
+  xlim(c(2200000, 7150000)) +
+  ylim(c(1380000, 5500000)) +
+  labs(
+    title = 'CO₂ Emissionen pro Einwohner in Europa', subtitle = '{frame_time}') +
+  transition_time(as.integer(year)) +
+  ease_aes('linear')
+
+
 
 # Draw Map
 ggplot(energy.map, aes(long, lat, group = group))+
   geom_polygon(aes(fill = energy_per_capita ), color = "white") +
-  transition_time(year) +
-  ease_aes('linear')
+
 
 animate(p1)
 anim_save('plot_gdpPercap_lifeExp.gif')
